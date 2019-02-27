032ebe72a12724bbffb35446fecf458a322d03fc
zhanz214 <zhanz214@mcmaster.ca>
2019-02-27 03:29:05 +0000

    project01 part2

--- Project01/project_analyze.sh
+++ Project01/project_analyze.sh
@@ -42,3 +42,49 @@ do
 done
 unset func
 exit
+
+#!/bin/bash
+
+MergeLog ()
+{
+         rm -f "merge.log"
+         log=`git log --oneline
+         while read -r line; do
+                 if [[ ! -z `echo $line | grep "Merge"` ]]; then
+                         hash=`echo $line | cut -d " " -f 1`
+                         echo $hash >> merge.log
+                         echo "Find it: $hash"
+                 fi
+         done <<< $log
+         echo "Finished"
+         echo "Go check "merge.log" in the same dir"
+}
+
+
+#!/bin/bash
+
+if [ $# -eq 1 ]
+then
+
+    if [$1 -eq 1 ]
+    then
+         grep -r --exclude="todo.log" "#TODO" > ~/CS1XA3/Project01/todo.log
+    elif [ $1 -eq 2 ]
+    then
+         echo "Compile Error log"
+         list1=$(find ~/CS1XA3 -name *.hs
+         list2=$(find ~/CS1XA3 -name *.py
+         for hsfi in $list1;do
+            cmp=$(ghci $hsfi)
+            if [ "$cmp" == *syntax errors* ]
+            then
+                echo $hsfi >> ~/CS1XA3/Project01/compile_fail.log
+            fi
+         done
+         for pyfi in $list2;do
+            cmp=$(python $pyfi)
+            if [ "$cmp"== *syntax errors* ]
+            then
+                echo $pyfi >> ~/CS1XA3/Project01/compile_fail.log
+            fi
+         done
